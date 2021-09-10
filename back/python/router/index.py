9674367875c4273c854a23ef59063d0e679c72ae
Xayah <1249475336@qq.com>
2021-09-10 16:32:50 +0800

    feat(back): 新增control接口

--- back/python/router/index.py
+++ back/python/router/index.py
@@ -17,7 +17,11 @@ def data():
         if request.method == 'POST':
             # temp=20&humi=0.8&lum=400
             form = request.form.to_dict()
-            _db.insert(form['temp'], form['humi'], form['lum'])
+            _db.insert('data', {
+                'temp': form['temp'],
+                'humi': form['humi'],
+                'lum': form['lum']
+            })
             return {
                 'code': 1,
                 'message': '成功插入一条数据',
@@ -25,7 +29,7 @@ def data():
             }
         else:
             args = request.args.to_dict()
-            data = _db.get_all()
+            data = _db.get_all('data')
             print(data)
             res = {
                 'code': 1,
@@ -49,6 +53,87 @@ def data():
                     'lum': data[len(data) - 1]['lum'],
                 }
             return res
+    except KeyError:
+        return {
+            'code': -1,
+            'message': '参数错误',
+        }
+
+
+@iot.route('/api/v1/control', methods=['GET', 'POST'])
+def control():
+    try:
+        if request.method == 'POST':
+            # obj=fan&action=1
+            form = request.form.to_dict()
+            print(form)
+            _db.insert('control', {
+                'obj': form['obj'],
+                'action': form['action'],
+            })
+            return {
+                'code': 1,
+                'message': '成功插入一条数据',
+                'data': form
+            }
+        else:
+            args = request.args.to_dict()
+            data = _db.get_all('control')
+            print(data)
+            res = {
+                'code': 1,
+                'data': [],
+            }
+            if args['get'] == 'all':
+                # /api/v1/control?get=all
+                for i in data:
+                    res['data'].append({
+                        'id': i['id'],
+                        'obj': i['obj'],
+                        'action': i['action'],
+                        'state': i['state'],
+                    })
+            if args['get'] == 'todo':
+                # /api/v1/control?get=todo
+                for i in data:
+                    if i['state'] == 0:
+                        res['data'].append({
+                            'id': i['id'],
+                            'obj': i['obj'],
+                            'action': i['action'],
+                            'state': i['state'],
+                        })
+            if args['get'] == 'finished':
+                # /api/v1/control?get=finished
+                for i in data:
+                    if i['state'] == 1:
+                        res['data'].append({
+                            'id': i['id'],
+                            'obj': i['obj'],
+                            'action': i['action'],
+                            'state': i['state'],
+                        })
+            return res
+    except KeyError:
+        return {
+            'code': -1,
+            'message': '参数错误',
+        }
+
+
+@iot.route('/api/v1/control/finish', methods=['POST'])
+def finish():
+    try:
+        if request.method == 'POST':
+            # id=1
+            form = request.form.to_dict()
+            print(form)
+            _db.update('control', form['id'])
+            return {
+                'code': 1,
+                'message': '成功更新一条数据',
+                'data': form
+            }
     except KeyError:
         return {
             'code': -1,
--- back/python/util/db.py
+++ back/python/util/db.py
@@ -4,15 +4,16 @@ HOST = 'db'  # 宿主机地址
 USER = 'root'  # 用户名
 PASSWORD = '123456'  # 密码
 DB = 'iotapp'  # 数据库名
-TABLE = 'data'  # 数据表名
+TABLE_DATA = 'data'  # 数据表名
+TABLE_CONTROL = 'control'  # 数据表名
 
 
-def get_all():
+def get_all(table):
     """返回表中所有数据"""
     db = pymysql.connect(host=HOST, user=USER, password=PASSWORD, charset='utf8')
     cursor = db.cursor(pymysql.cursors.DictCursor)
     cursor.execute("use {};".format(DB))
-    cursor.execute("SELECT * from {}".format(TABLE))
+    cursor.execute("SELECT * from {}".format(table))
     data = cursor.fetchall()
     db.close()
     return data
@@ -31,15 +32,33 @@ def init():
         "CREATE TABLE IF NOT EXISTS {}(id int primary key not null auto_increment,"
         "temp float,"
         "humi float,"
-        "lum float);".format(TABLE))
+        "lum float);".format(TABLE_DATA))
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS {}(id int primary key not null auto_increment,"
+        "obj text,"
+        "action tinyint,"
+        "state tinyint);".format(TABLE_CONTROL))
     db.close()
 
 
-def insert(temp, humi, lum):
+def insert(table, data):
     """插入一条记录"""
     db = pymysql.connect(host=HOST, user=USER, password=PASSWORD, charset='utf8')
     cursor = db.cursor()
     cursor.execute("use {};".format(DB))
-    cursor.execute("INSERT INTO data VALUES (0,{},{},{})".format(temp, humi, lum))
+    if table == TABLE_DATA:
+        cursor.execute("INSERT INTO {} VALUES (0,{},{},{})".format(table, data['temp'], data['humi'], data['lum']))
+    elif table == TABLE_CONTROL:
+        cursor.execute("INSERT INTO {} VALUES (0,'{}',{},{})".format(table, data['obj'], data['action'], 0))
+    db.commit()
+    db.close()
+
+
+def update(table, id):
+    """插入一条记录"""
+    db = pymysql.connect(host=HOST, user=USER, password=PASSWORD, charset='utf8')
+    cursor = db.cursor()
+    cursor.execute("use {};".format(DB))
+    cursor.execute("update {} set state = {} where id = {}".format(table, 1, id))
     db.commit()
     db.close()
