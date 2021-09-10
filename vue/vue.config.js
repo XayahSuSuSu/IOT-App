2ee7d17937b1651acb1e43015ea75dd4d4a84e0a
Xayah <1249475336@qq.com>
2021-09-10 23:41:44 +0800

    feat(vue): 添加跨域代理

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3307',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    }
+  }
 }
