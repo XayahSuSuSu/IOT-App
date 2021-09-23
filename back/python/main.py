ad85b99872665423c74bb35d99f949a2bceb39d7
Xayah <zds1249475336@gmail.com>
2021-09-23 20:12:46 +0800

    fix(back): 修复接口中文乱码问题

--- back/python/main.py
+++ back/python/main.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 from router.index import iot
 
 app.register_blueprint(iot, url_prefix="/")
 
 if __name__ == '__main__':
-    app.config['JSON_AS_ASCII'] = False
     app.run()
