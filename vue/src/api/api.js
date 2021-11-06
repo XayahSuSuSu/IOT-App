b83b91b614147c5c991cf85976bb372a8b2ae8d3
Xayah <zds1249475336@gmail.com>
2021-11-06 14:35:53 +0800

    feat(vue): 适配打包后api调用需求

--- vue/src/api/api.js
+++ vue/src/api/api.js
@@ -1,32 +1,34 @@
 import './requests'
 import {get, post} from '@/api/requests'
 
+const host = 'http://192.168.1.124:3307'
+
 /**
  * 获取最新的一条data记录
  */
 export async function getLatestData() {
-    return await get('/api/v1/data?get=latest')
+    return await get(`${host}/api/v1/data?get=latest`)
 }
 
 /**
  * 获取历史data记录
  */
 export async function getAllData() {
-    return await get('/api/v1/data?get=all')
+    return await get(`${host}/api/v1/data?get=all`)
 }
 
 /**
  * 获取控制记录
  */
 export async function getAllControlData() {
-    return await get('/api/v1/control?get=all')
+    return await get(`${host}/api/v1/control?get=all`)
 }
 
 /**
  * 添加一条控制记录
  */
 export async function addControlData(protocol) {
-    return await post('/api/v1/control', {
+    return await post(`${host}/api/v1/control`, {
         protocol: protocol,
         state: ''
     })
