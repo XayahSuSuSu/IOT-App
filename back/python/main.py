651bf2dd0cfb4d07997ba598f9d23ad9b1e165f5
Xayah <zds1249475336@gmail.com>
2021-11-06 14:34:48 +0800

    fix(back): 支持跨域请求

--- back/python/main.py
+++ back/python/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+CORS(app, supports_credentials=True)
 
 from router.index import iot
 
